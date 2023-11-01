b683e68181d342b2f22fc61d43f5b259e261a9d6
Alex <alex.streeter@airlockdigital.com>
2023-11-01 21:34:43 +1030

    clean up

--- dom/dom.go
+++ dom/dom.go
@@ -5,7 +5,6 @@
 package dom
 
 import (
-	"log"
 	"syscall/js"
 )
 
@@ -31,31 +30,15 @@ type Tager interface {
 }
 
 func emptyRender(t Tager) string {
-	log.Printf("this should be fine t: %v %T", t, t)
-	if t == nil {
-		return "<div></div>"
-	}
-
 	return t.OpenTag() + t.CloseTag()
 }
 
 // Render traverses a node tree and generates a string of that node
 // including all renderings of child nodes
 func Render(node Node) string {
-	log.Printf("processing node: %v is it nil %v", node, node == nil)
 	if node.IsNil() {
-		log.Printf("empty node early exit: %v", node)
 		return emptyRender(node)
 	}
-
-	if h, ok := node.(*Header); ok {
-		if h == nil {
-			log.Printf("I can see it's nil when I convert its type")
-		} else {
-			log.Printf("h is %#v", h)
-		}
-	}
-	log.Printf("node is not nil it is: %v %T", node, node)
 
 	nr := node.OpenTag()
 
@@ -76,19 +59,13 @@ func Render(node Node) string {
 // body.innerHTML = getHTML()
 // this is the default behaviour for raft.
 func (d *DOM) Render() {
-	log.Printf("start render process")
 	htmlString := ""
 
 	// TODO: currently we are updating only the body through the assets
 	// fix this so that we can also set the headers
-	log.Printf("processing header: %v", d.Header)
-	log.Printf("is header nil: %v", d.Header == nil)
 	htmlString += Render(d.Header)
-
-	log.Printf("processing body: %v", d.Body)
 	htmlString += Render(d.Body)
 
-	log.Printf("about to render htmlString %v", htmlString)
 	js.Global().Set("getHtml", getHtml(htmlString))
 }
 
--- dom/header.go
+++ dom/header.go
@@ -4,8 +4,8 @@ type Header struct {
 	Element
 }
 
-func (h *Header) OpenTag() string  { return "<header>" }
-func (h *Header) CloseTag() string { return "</header>" }
+func (h *Header) OpenTag() string  { return "<head>" }
+func (h *Header) CloseTag() string { return "</head>" }
 func (h *Header) IsNil() bool {
 	return nil == h
 }
